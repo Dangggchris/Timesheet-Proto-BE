dbf1dcaa012371aa9633ba69850b65ca9dd4bd5a
tflunk <45830707+taydougherty@users.noreply.github.com>
2020-09-17 13:29:57 -0700

    made daily timesheet model, migration and controller file

---
+++ database/migrations/2020_09_17_202003_daily_timesheet_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DailyTimesheetTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('dailytimesheet', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('project_id');
+                $table->integer('user_id');
+                $table->text('date');
+                $table->float('hours', 3, 1);
+                $table->text('notes');
+                $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dailytimesheet');
+    }
+}
