2078a9b5dcd34fb551d7d36f1bfc9eb848c72186
Cedric Orejola <cedricorejola@gmail.com>
2020-10-01 11:56:47 -0700

    updated Cors.php headers

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,11 +18,13 @@ class Cors
     {
         // header('Access-Control-Allow-Headers: Content-type, X-Auth-Token, Authorization, Origin');
         // header('Access-Control-Allow-Origin', '*');
-        // header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
-        header('Access-Control-Allow-Origin:  http://localhost:4200');
-        header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
-        return $next($request);
+        // // header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        // ->header('Access-Control-Allow-Origin:  http://localhost:4200');
+        // ->header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
+        // ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+        return $next($request)
+            ->header('Allow-Control-Allow-Origin', 'http://localhost:4200')
+            ->header('Acces-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Auth-Token, Origin');
     }
 }
