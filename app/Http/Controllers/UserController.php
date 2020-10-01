6c3810d8b543dd5ff59b9868134d4dccbd79beb1
Cedric Orejola <cedricorejola@gmail.com>
2020-10-01 15:56:08 -0700

    saving

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,65 +11,63 @@ use Carbon\Carbon;
 
 class UserController extends Controller
 {
-// You should ensure that all datas received are valid and secure.
+  // You should ensure that all datas received are valid and secure.
 
-public function login(Request $request) {
-  
-  // Launch Firebase Auth
-  $auth = app('firebase.auth');
-  // Retrieve the Firebase credential's token
-  $idTokenString = $request->input('Firebasetoken');
+  public function login(Request $request)
+  {
 
-  
-  try { // Try to verify the Firebase credential token with Google
-    
-    $verifiedIdToken = $auth->verifyIdToken($idTokenString);
-    
-  } catch (\InvalidArgumentException $e) { // If the token has the wrong format
-    
-    return response()->json([
+    // Launch Firebase Auth
+    $auth = app('firebase.auth');
+    // Retrieve the Firebase credential's token
+    $idTokenString = $request->input('Firebasetoken');
+
+
+    try { // Try to verify the Firebase credential token with Google
+
+      $verifiedIdToken = $auth->verifyIdToken($idTokenString);
+    } catch (\InvalidArgumentException $e) { // If the token has the wrong format
+
+      return response()->json([
         'message' => 'Unauthorized - Can\'t parse the token: ' . $e->getMessage()
-    ], 401);        
-    
-  } catch (InvalidToken $e) { // If the token is invalid (expired ...)
-    
-    return response()->json([
+      ], 401);
+    } catch (InvalidToken $e) { // If the token is invalid (expired ...)
+
+      return response()->json([
         'message' => 'Unauthorized - Token is invalide: ' . $e->getMessage()
-    ], 401);
-    
+      ], 401);
+    }
+
+    // Retrieve the UID (User ID) from the verified Firebase credential's token
+    $uid = $verifiedIdToken->getClaim('sub');
+
+    // Retrieve the user model linked with the Firebase UID
+    $user = User::where('firebaseUID', $uid)->first();
+
+    // Here you could check if the user model exist and if not create it
+    // For simplicity we will ignore this step
+
+    // Once we got a valid user model
+    // Create a Personnal Access Token
+    $tokenResult = $user->createToken('Personal Access Token');
+
+    // Store the created token
+    $token = $tokenResult->token;
+
+    // Add a expiration date to the token
+    $token->expires_at = Carbon::now()->addWeeks(1);
+
+    // Save the token to the user
+    $token->save();
+
+    // Return a JSON object containing the token datas
+    // You may format this object to suit your needs
+    return response()->json([
+      'id' => $user->id,
+      'access_token' => $tokenResult->accessToken,
+      'token_type' => 'Bearer',
+      'expires_at' => Carbon::parse(
+        $tokenResult->token->expires_at
+      )->toDateTimeString()
+    ]);
   }
-
-  // Retrieve the UID (User ID) from the verified Firebase credential's token
-  $uid = $verifiedIdToken->getClaim('sub');
-
-  // Retrieve the user model linked with the Firebase UID
-  $user = User::where('firebaseUID',$uid)->first();
-  
-  // Here you could check if the user model exist and if not create it
-  // For simplicity we will ignore this step
-
-  // Once we got a valid user model
-  // Create a Personnal Access Token
-  $tokenResult = $user->createToken('Personal Access Token');
-  
-  // Store the created token
-  $token = $tokenResult->token;
-  
-  // Add a expiration date to the token
-  $token->expires_at = Carbon::now()->addWeeks(1);
-  
-  // Save the token to the user
-  $token->save();
-  
-  // Return a JSON object containing the token datas
-  // You may format this object to suit your needs
-  return response()->json([
-    'id' => $user->id,
-    'access_token' => $tokenResult->accessToken,
-    'token_type' => 'Bearer',
-    'expires_at' => Carbon::parse(
-      $tokenResult->token->expires_at
-    )->toDateTimeString()
-  ]);
-}
 }
