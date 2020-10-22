67050d0a1ec27c13f886ab6cf07ff10edb707ad7
Dangggchris <christophermtdang@gmail.com>
2020-10-22 11:35:29 -0700

    successfully added user to database in the usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,16 +43,19 @@ class UserController extends Controller
 
     $token= $idTokenString;
 
-    $key = "BIu0LHGioSeBBSvWHXcTE6WLLjt6dZVaq3LpvexP2tkRw99y3x79SjtfBmbg6mnmRVPUrNtKfBH2Y03q_AFmPkw";
+    $decoded = json_decode(base64_decode(str_replace('_', '/', str_replace('-', '+', explode('.', $token)[1]))));
 
-    $jwt = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjBlM2FlZWUyYjVjMDhjMGMyODFhNGZmN2RjMmRmOGIyMzgyOGQ1YzYiLCJ0eXAiOiJKV1QifQ.eyJuYW1lIjoiQ2VkcmljIE9yZWpvbGEiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EtL0FPaDE0R2hhZ0lJbV9ZN3M3UWhwejRlRHhOQjJuNEJjWEdTd1pCd092N0lRZ3ciLCJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vdmhiLXRpbWVzaGVldC1wcm90byIsImF1ZCI6InZoYi10aW1lc2hlZXQtcHJvdG8iLCJhdXRoX3RpbWUiOjE2MDMzMTgzOTYsInVzZXJfaWQiOiJVbjhON3c3SG4wZTJoa3A0N2YySFhSNDVteXYyIiwic3ViIjoiVW44Tjd3N0huMGUyaGtwNDdmMkhYUjQ1bXl2MiIsImlhdCI6MTYwMzMxODM5NiwiZXhwIjoxNjAzMzIxOTk2LCJlbWFpbCI6ImNlZHJpY29yZWpvbGFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMTMwNjU0NjE5MzYwODk5ODY5MTYiXSwiZW1haWwiOlsiY2Vkcmljb3Jlam9sYUBnbWFpbC5jb20iXX0sInNpZ25faW5fcHJvdmlkZXIiOiJnb29nbGUuY29tIn19.ULd5LQsUxfj5-gCBoTzmoa-b38fe9f_wz8h8a8igc9w92GO9znhuaeBSM73B9uokOvZn3DmYyQy7MoLqwdrO1kuHD6yLHHlnhkuxbDrBAaYMKCbbGfoL3GrC47VbcqfZ23VRVwoh4NRV2oXGRE1oYkWpRi3npLKJ2NFAJe5cW6DJWjEa6lF--WJvGrJbaUy1zblVdcAmP-9FPmcyMFamDRQKejNEuur1DKmsABc6P_hpMwEOpSf0VpjpJSfichMoSkBT41vmyHZOyISsk9zIHnLIOiIonCWTkGXqqw80dPLZeQSfhXKj6aiZSv17vS3AG47EM29_hp2K0AwuaenI8Q";
+    $googleID = $decoded->user_id;
+    // $googleName = $decoded->displayName;
 
-    $decoded = JWT::decode($idTokenString, $key, array('RS256'));
+    $user = User::firstOrCreate(
+      ['guid' => $googleID]
+    );
 
-    print_r($decoded);
+    $user_id = $user->id;
 
     return response()->json([
-      // 'id' => $id,
+      'user_id' => $user_id,
       'access_token' => $token,
       'token_type' => 'Bearer',
       // 'expires_at' => Carbon::parse(
