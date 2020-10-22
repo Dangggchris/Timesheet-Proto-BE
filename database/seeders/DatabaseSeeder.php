fdaa7e6a2e1376aa10b3bbbbb75c5a3383e8e4dc
Dangggchris <christophermtdang@gmail.com>
2020-10-22 15:45:10 -0700

    returned user_id variable instead of json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserTableSeeder::class);
         $this->call(ProjectsTableSeeder::class);
         $this->call(DailyTimesheetSeeder::class);
     }
