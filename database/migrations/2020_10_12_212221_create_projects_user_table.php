13bc453647adaec81d50fa361d0fe0f97e6e870c
Cedric Orejola <cedricorejola@gmail.com>
2020-11-10 09:52:40 -0800

    updated projects_users pivot table

--- database/migrations/2020_10_12_212221_create_projects_user_table.php
+++ database/migrations/2020_10_12_212221_create_projects_user_table.php
@@ -15,13 +15,14 @@ class CreateProjectsUserTable extends Migration
     {
         Schema::create('projects_user', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('projects_id');
-            // SQLSTATE[42000]: Syntax error or access violation: 1068 Multiple primary key defined (SQL: alter table `projects_user` add primary key `projects_user_user_id_projects_id_primary`(`user_id`, `projects_id`))
-            // $table->primary(['user_id', 'projects_id']);
 
-            $table->foreign('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
-            $table->foreign('projects_id')->references('id')->on('projects')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->unsignedBigInteger('projects_id');
+            $table->foreign('projects_id')->references('id')->on('projects')->onDelete('cascade');
+
+            $table->primary(['user_id', 'projects_id']);
         });
     }
 
