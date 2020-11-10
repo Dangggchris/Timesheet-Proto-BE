dfeec6a1638b6b06fc869bdef43de5c0dbb277ce
Cedric Orejola <cedricorejola@gmail.com>
2020-11-09 16:07:12 -0800

    updated database.php

--- config/database.php
+++ config/database.php
@@ -10,6 +10,15 @@ $username = $dbparts['user'];
 $password = $dbparts['pass'];
 $database = ltrim($dbparts['path'], '/');
 
+try {
+    $conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
 return [
 
     /*
