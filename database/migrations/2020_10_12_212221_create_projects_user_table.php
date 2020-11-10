70d66b169c564d7c83dbb5b7380a3edc43916f0d
Cedric Orejola <cedricorejola@gmail.com>
2020-11-10 09:43:01 -0800

    updated projects_users pivot table

--- database/migrations/2020_10_12_212221_create_projects_user_table.php
+++ database/migrations/2020_10_12_212221_create_projects_user_table.php
@@ -20,9 +20,9 @@ class CreateProjectsUserTable extends Migration
 
             // $table->primary(['user_id', 'projects_id']);
 
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('projects_id')->references('id')->on('projects');
-            // $table->foreign('projects_user_user_id_foreign')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('projects_id')->references('id')->on('projects')->onDelete('cascade');
+            $table->foreign('projects_user_user_id_foreign')->references('id')->on('users');
         });
     }
 
