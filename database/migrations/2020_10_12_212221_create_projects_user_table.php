6e1263edb9483438e746f808cc3bbb663fec9324
Cedric Orejola <cedricorejola@gmail.com>
2020-11-10 09:48:38 -0800

    updated projects_users pivot table

--- database/migrations/2020_10_12_212221_create_projects_user_table.php
+++ database/migrations/2020_10_12_212221_create_projects_user_table.php
@@ -17,8 +17,8 @@ class CreateProjectsUserTable extends Migration
             $table->increments('id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('projects_id');
-
-            $table->primary(['user_id', 'projects_id']);
+            // SQLSTATE[42000]: Syntax error or access violation: 1068 Multiple primary key defined (SQL: alter table `projects_user` add primary key `projects_user_user_id_projects_id_primary`(`user_id`, `projects_id`))
+            // $table->primary(['user_id', 'projects_id']);
 
             $table->foreign('user_id')->references('id')->on('users')->constrained()->onDelete('cascade');
             $table->foreign('projects_id')->references('id')->on('projects')->constrained()->onDelete('cascade');
