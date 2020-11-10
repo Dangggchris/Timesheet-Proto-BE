17d003ae60b9416355b9abc480d295743c018e07
Cedric Orejola <cedricorejola@gmail.com>
2020-11-09 17:18:18 -0800

    updated projects_users pivot table

--- database/migrations/2020_10_12_212221_create_projects_user_table.php
+++ database/migrations/2020_10_12_212221_create_projects_user_table.php
@@ -14,13 +14,14 @@ class CreateProjectsUserTable extends Migration
     public function up()
     {
         Schema::create('projects_user', function (Blueprint $table) {
+            $table->increments('id');
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('projects_id');
 
-            $table->primary(['user_id', 'projects_id']);
+            // $table->primary(['user_id', 'projects_id']);
 
-            $table->foreign('user_id')->references('users')->on('id')->onDelete('cascade');
-            $table->foreign('projects_id')->references('projects')->on('id')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('projects_id')->references('id')->on('projects')->onDelete('cascade');
             // $table->foreign('projects_user_user_id_foreign')->references('id')->on('users');
         });
     }
